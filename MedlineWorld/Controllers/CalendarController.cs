e6b920ba5278337f114ff9b1af0a165d4795d358
Thienvu3395 <thienvu3395@gmail.com>
2021-05-30 16:19:13 +0700

    Add project files.

---
+++ MedlineWorld/Controllers/CalendarController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MedlineWorld.Controllers
+{
+    public class CalendarController : Controller
+    {
+        // GET: Calendar
+        public ActionResult Index() // MedlineWorld
+        {
+            return View();
+        }
+        public ActionResult MedlineVN()
+        {
+            return View();
+        }
+        public ActionResult Manage()
+        {
+            return View();
+        }
+    }
+}
