29c40447c40fdfb99cfd95559fa8b51df9b92aa1
Thienvu3395 <thienvu3395@gmail.com>
2021-06-03 11:53:02 +0700

    Cập nhật giao diện tỉnh module "tài liệu"

---
+++ MedlineWorld/Controllers/DocumentController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MedlineWorld.Controllers
+{
+    public class DocumentController : Controller
+    {
+        // GET: Document
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Manage()
+        {
+            return View();
+        }
+    }
+}
